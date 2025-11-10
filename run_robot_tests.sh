7af9d3ec60107cef87a322dbd1597ea43cae409c
Mullil <eetu.taatila@gmail.com>
2025-11-10 11:27:33 +0200

    fix action

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -7,12 +7,14 @@ poetry run python3 src/index.py &
 
 echo "started Flask server"
 
-# odetetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
+# odotetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5001)" != "200" ]];
   do sleep 1;
 done
 
 echo "Flask server is ready"
+
+export CHROME_OPTIONS="--headless --no-sandbox --disable-dev-shm-usage"
 
 # suoritetaan testit
 poetry run robot --variable HEADLESS:true src/tests
